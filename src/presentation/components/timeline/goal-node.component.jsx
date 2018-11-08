365cd440243461564d2adc45a4270838c7dd9884
Admas Molla <molla.admas@gmail.com>
2018-11-07 18:30:14 -0800

    removing goals assignment, in favor k:v descriptions

--- src/presentation/components/timeline/goal-node.component.jsx
+++ src/presentation/components/timeline/goal-node.component.jsx
@@ -24,21 +24,12 @@ class GoalNode extends Component {
       <Card>
         <CardTitle
           title={this.props.node.data('label')}
-          // actAsExpander
-          // showExpandableButton
+          actAsExpander
+          showExpandableButton
         />
-        // <CardActions>
-        //   <FlatButton className='btn btn-default btn-success' style={styles.completed} label='Completed' />
-        //   <FlatButton className='btn btn-default btn-warning' style={styles.active} label='Active' />
-        //   <FlatButton className='btn btn-default btn-hold' style={styles.hold} label='On Hold' />
-        //   <FlatButton className='btn btn-default btn-danger' style={styles.discontinued} label='Discontinued' />
-        // </CardActions>
-        // <CardText expandable>
-        //   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        //   Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        //   Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        //   Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-        // </CardText>
+        <CardText expandable>
+          {JSON.stringify(this.props.node.data())}
+        </CardText>
       </Card>
     )
   }
